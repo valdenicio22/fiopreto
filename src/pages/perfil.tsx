6edc2502b5a2452d9e0bfa33e16c274dc7d775a0
valdenicio22 <valdenicio22@gmail.com>
2021-05-29 13:44:13 -0300

    adding perfil page with drawer

--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -1,3 +1,45 @@
+import Button from '@material-ui/core/Button';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import TemporaryDrawer from '../components/Drawer';
+import { FullLogoIcon } from '../components/Icons';
+import styles from '../styles/perfil.module.scss';
+
 export default function Perfil() {
-  return <h1>PerfilPage</h1>;
+  return (
+    <div className={styles.perfilContainer}>
+      <div className={styles.perfilHeader}>
+        <FullLogoIcon />
+      </div>
+      <div className={styles.perfilDrawer}>
+        <h2>Maria da Silva</h2>
+        <TemporaryDrawer />
+      </div>
+      <div className={styles.perfilInformations}>
+        <Button
+          className={styles.perfilBtn}
+          variant="outlined"
+          color="primary"
+          endIcon={<ArrowForwardIcon />}
+        >
+          Dados pessoais
+        </Button>
+        <Button
+          className={styles.perfilBtn}
+          variant="outlined"
+          color="primary"
+          endIcon={<ArrowForwardIcon />}
+        >
+          Dados da empresa
+        </Button>
+        <Button
+          className={styles.perfilBtn}
+          variant="outlined"
+          color="primary"
+          endIcon={<ArrowForwardIcon />}
+        >
+          Foto e horários
+        </Button>
+      </div>
+    </div>
+  );
 }
